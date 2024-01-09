5cd3dd7e0c8ef745a95bb8d58d992ed3aaf48dba
waltherbaasch <U2444127@rts-365.dk>
2024-01-09 20:02:24 +0100

    added buttons

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HeartIcon } from '@heroicons/react/solid';
+
+const Footer = () => {
+  return (
+    <footer className="w-full h-16 bg-white flex items-center justify-between p-4">
+
+      <div className="flex items-center space-x-4 flex-grow justify-center">
+       
+        <imported-icon-1 className="text-gray-500 w-6 h-6" />
+
+        
+        <imported-icon-2 className="text-gray-500 w-6 h-6" />
+
+
+        <imported-icon-3 className="text-gray-500 w-6 h-6" />
+      </div>
+
+     
+      <div className="w-375 h-67"></div>
+    </footer>
+  );
+};
+
+export default Footer;
