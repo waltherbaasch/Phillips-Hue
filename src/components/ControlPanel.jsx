cf2294642b4ade67b18c890b46fb45da5710ced0
waltherbaasch <U2444127@rts-365.dk>
2024-01-11 11:10:12 +0100

    Added names

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -4,27 +4,27 @@ const ControlPanel = () => {
   return (
     <div className="flex flex-wrap justify-center">
       <div className="w-36 h-36 rounded-md bg-blue-500 m-4 flex items-center justify-center text-white">
-        Box 1
+       <h1>Bedroom</h1>
       </div>
 
       <div className="w-36 h-36 rounded-md bg-green-500 m-4 flex items-center justify-center text-white">
-        Box 2
+       <h1>Living room</h1>
       </div>
 
       <div className="w-36 h-36 rounded-md bg-yellow-500 m-4 flex items-center justify-center text-white">
-        Box 3
+        <h1>Kitchen</h1>
       </div>
 
       <div className="w-36 h-36 rounded-md bg-red-500 m-4 flex items-center justify-center text-white">
-        Box 4
+        <h1>Bathroom</h1>
       </div>
 
       <div className="w-36 h-36 rounded-md bg-purple-500 m-4 flex items-center justify-center text-white">
-        Box 5
+      <h1>Outdoor</h1>
       </div>
 
       <div className="w-36 h-36 rounded-md bg-pink-500 m-4 flex items-center justify-center text-white">
-        Box 6
+      <h1>Balcony</h1>
       </div>
     </div>
   );
