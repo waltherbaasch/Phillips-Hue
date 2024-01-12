a2a966b8067395e3cd74e92ea47f1a05d968f1ea
waltherbaasch <U2444127@rts-365.dk>
2024-01-12 12:34:07 +0100

    added switch

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const ControlPanel = () => {
   return (
     <div className="flex flex-wrap justify-center">
-      <div className="w-36 h-36 rounded-md bg-blue-500 m-4 flex items-center justify-center text-white">
-       <h1>Bedroom</h1>
+      <div className="w-150 h-150 rounded-md bg-blue-500 m-4 flex items-center justify-center text-white">
+        <img src="bedroom_image.jpg" alt="Bedroom" width="51" height="42" />
+        <h1>Bedroom</h1>
       </div>
 
-      <div className="w-36 h-36 rounded-md bg-green-500 m-4 flex items-center justify-center text-white">
-       <h1>Living room</h1>
+      <div className="w-150 h-150 rounded-md bg-green-500 m-4 flex items-center justify-center text-white">
+        <img src="living_room_image.jpg" alt="Living room" width="51" height="42" />
+        <h1>Living room</h1>
       </div>
 
-      <div className="w-36 h-36 rounded-md bg-yellow-500 m-4 flex items-center justify-center text-white">
+      <div className="w-150 h-150 rounded-md bg-yellow-500 m-4 flex items-center justify-center text-white">
+        <img src="kitchen_image.jpg" alt="Kitchen" width="51" height="42" />
         <h1>Kitchen</h1>
       </div>
 
-      <div className="w-36 h-36 rounded-md bg-red-500 m-4 flex items-center justify-center text-white">
+      <div className="w-150 h-150 rounded-md bg-red-500 m-4 flex items-center justify-center text-white">
+        <img src="bathroom_image.jpg" alt="Bathroom" width="51" height="42" />
         <h1>Bathroom</h1>
       </div>
 
-      <div className="w-36 h-36 rounded-md bg-purple-500 m-4 flex items-center justify-center text-white">
-      <h1>Outdoor</h1>
+      <div className="w-150 h-150 rounded-md bg-purple-500 m-4 flex items-center justify-center text-white">
+        <img src="outdoor_image.jpg" alt="Outdoor" width="51" height="42" />
+        <h1>Outdoor</h1>
       </div>
 
-      <div className="w-36 h-36 rounded-md bg-pink-500 m-4 flex items-center justify-center text-white">
-      <h1>Balcony</h1>
+      <div className="w-150 h-150 rounded-md m-4 flex items-center justify-center text-white">
+        <img src="balcony_image.jpg" alt="Balcony" width="51" height="42" />
+        <h1>Balcony</h1>
       </div>
     </div>
   );
---
+++ src/components/IntensitySwitch.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function IntensitySwitch() {
+  return (
+    <>
+      <p>Light Intensity settings:</p>
+
+      <div>
+        <input type="range" id="intensity" name="intensity" min="0" max="11" />
+        <label htmlFor="intensity">Intensity</label>
+      </div>
+
+      <div>
+        <input
+          type="range"
+          id="sunshine"
+          name="sunshine"
+          min="0"
+          max="100"
+          value="90"
+          step="10"
+        />
+        <label htmlFor="sunshine">Sunshine</label>
+      </div>
+    </>
+  );
+}
+
+export default IntensitySwitch;
